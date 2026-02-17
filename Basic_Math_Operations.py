1245710cb2531b27d2b9f0788db2880ad6aec7f7
mukulbhardwaj750 <mukulbhardwaj8888@gmail.com>
2026-02-17 15:46:21 +0530

    Initial commit

---
+++ Basic_Math_Operations.py
@@ -0,0 +1,7 @@
+First_number = int(input("Enter the first number: "))
+Second_number = int(input("Enter the second number: "))
+
+print("Addition: ", First_number + Second_number)
+print("Subtraction: ", First_number - Second_number)
+print("Multiplication: ", First_number * Second_number)
+print("Division: ", First_number / Second_number)
